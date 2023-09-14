0a4d2045d1b3cc1342e55f83baf7597121611053
Selcuk KARAKAS <selcukkarakas1@gmail.com>
2023-09-14 15:25:11 +0000

    Week 05 added

--- src/week_05/assignments/Question_05_39.java
+++ src/week_05/assignments/Question_05_39.java
@@ -1,4 +1,35 @@
 package week_05.assignments;
 
 public class Question_05_39 {
+
+
+        public static void main(String[] args) {
+            // Create a consstant value for commmission sought
+            final double COMMISSION_SOUGHT = 30000;
+            double salesAmount, 				// Sales amount
+                    commission,				// Employee commission
+                    balance;					// Sales balance
+            salesAmount = 0;					// Initialize accumulator to 0
+            do {
+                balance = commission = 0;	// Set balance and commission to 0
+                salesAmount += 0.01;			// Increase sales amount by $0.01
+
+                // If sales amount is $10,000.01 and above commission rate is 12%
+                if (salesAmount > 10000)
+                    commission += (balance = salesAmount - 10000) * 0.12;
+
+                // If sales amount is $5,000.01-$10,000 commission rate is 10%
+                if (salesAmount > 5000)
+                    commission += (balance -= balance - 5000) * 0.10;
+
+                // If sales amount is $0.01-$5,000 commission rate is 8%
+                if (salesAmount > 0)
+                    commission += balance * 0.08;
+
+                // While commission is less than commission sought loop
+            } while (commission < COMMISSION_SOUGHT);
+
+            // Display results
+            System.out.printf("Minimum sales to earn $30,000: $%.0f\n", salesAmount);
+        }
 }
--- src/week_05/assignments/Question_05_40.java
+++ src/week_05/assignments/Question_05_40.java
@@ -1,4 +1,26 @@
 package week_05.assignments;
 
 public class Question_05_40 {
+
+
+        public static void main(String[] args) {
+            int heads, 				// Counts the number of heads
+                    tails, 				// Counts the number of tails
+                    coin; 				// Coin
+            heads = tails = 0;	// Initialize heads and tails to 0;
+
+            // Simulate flipping a coin one million times
+            System.out.println("Flipping a coin one million times..");
+            for (int i = 0; i < 1000000; i++) {
+                // Generate randomly 0 or 1.
+                // If 0 increament heads otherwise increment tails
+                coin = (int)(Math.random() * 2) == 0 ? heads++ : tails++;
+            }
+
+            // Display results
+            System.out.println(
+                    "Heads: " + heads +
+                            "\nTails: " + tails);
+        }
+
 }
