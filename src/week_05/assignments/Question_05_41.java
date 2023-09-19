f62edb49d5b29c6b820d1dd6a945cb774ca8d072
Selcuk KARAKAS <selcukkarakas1@gmail.com>
2023-09-19 12:33:15 +0000

    Week 05 added

--- src/week_05/assignments/Question_05_41.java
+++ src/week_05/assignments/Question_05_41.java
@@ -1,4 +1,47 @@
 package week_05.assignments;
 
+import java.util.Scanner;
 public class Question_05_41 {
+
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+
+            int max = 0;  // Initialize max to 0
+            int count = 0;  // Initialize count to 0
+
+            System.out.println("Enter integers (end with 0):");
+
+            int num = input.nextInt();
+
+            // Check if the first number is not 0
+            if (num != 0) {
+                max = num;
+                count = 1;
+            }
+
+            while (num != 0) {
+                num = input.nextInt();
+
+                if (num == max) {
+                    count++;
+                } else if (num > max) {
+                    max = num;
+                    count = 1;
+                }
+            }
+
+            if (max != 0) {
+                System.out.println("The largest number is: " + max);
+                System.out.println("The occurrence count for " + max + " is: " + count);
+            } else {
+                System.out.println("No valid numbers were entered.");
+            }
+
+            input.close();
+        }
+
+
 }
+
+
+
--- src/week_05/assignments/Question_05_46.java
+++ src/week_05/assignments/Question_05_46.java
@@ -1,4 +1,29 @@
 package week_05.assignments;
 
+import java.util.Scanner;
 public class Question_05_46 {
-}
+
+    /*
+(Reverse a string) Write a program that prompts the user to enter a string and
+displays the string in reverse order.
+*/
+
+
+
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+
+            // Prompt the user to enter a string
+            System.out.print("Enter a string: ");
+            String string = input.nextLine();
+
+            // Reverse the string
+            String reverse = "";
+            for (int i = string.length() - 1; i >= 0; i--) {
+                reverse += string.charAt(i);
+            }
+
+            // Display the string in reverse order
+            System.out.println("The reversed string is " + reverse);
+        }
+    }
