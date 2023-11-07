d8b797b3141794fd41c5d4217ddbf84a1bb31f51
Selcuk KARAKAS <selcukkarakas1@gmail.com>
2023-11-07 16:00:15 +0300

    Week 09 added.

--- src/week_09/assignments/Question_09_11.java
+++ src/week_09/assignments/Question_09_11.java
@@ -1,4 +1,37 @@
 package week_09.assignments;
 
+import java.util.Scanner;
 public class Question_09_11 {
+
+
+
+
+        /** Main method */
+        public static void main(String[] args) {
+            // Create a Scanner object
+            Scanner input = new Scanner(System.in);
+
+
+            // Prompt the user to enter a, b, c, d, e, and f
+            System.out.print("Enter a, b, c, d, e, f: ");
+            double a = input.nextDouble();
+            double b = input.nextDouble();
+            double c = input.nextDouble();
+            double d = input.nextDouble();
+            double e = input.nextDouble();
+            double f = input.nextDouble();
+
+            // Create a LinearEquation object
+            LinearEquation linearEquation = new LinearEquation(a, b, c, d, e, f);
+
+            // Display results
+            if (linearEquation.isSolvable()) {
+                System.out.println("x is " + linearEquation.getX() +
+                        " and y is " + linearEquation.getY());
+            }
+            else
+                System.out.println("The equation has no solution.");
+        }
+
+
 }
--- src/week_09/assignments/Question_09_13.java
+++ src/week_09/assignments/Question_09_13.java
@@ -1,4 +1,41 @@
 package week_09.assignments;
 
+import java.util.Scanner;
 public class Question_09_13 {
+
+
+
+
+        /** Main method */
+        public static void main(String[] args) {
+            // Create a Scanner object
+            Scanner input = new Scanner(System.in);
+
+            // Prompt the user to enter a two-dimensional array
+            System.out.print("Enter the number of rows and columns in the array: ");
+            int rows = input.nextInt();
+            int columns = input.nextInt();
+
+            // Create a matrix
+            double[][] array = new double[rows][columns];
+            System.out.println("Enter the array: ");
+            for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j < array[i].length; j++) {
+                    array[i][j] = input.nextDouble();
+                }
+            }
+
+            // Get and instance of Loacation
+            Location max = locateLargest(array);
+
+            // Display the location of the largest element in the array
+            System.out.println("The location of the largest element is " +
+                    max.maxValue + " at (" + max.row + ", " + max.column + ")");
+        }
+
+        /** locateLargest returns an instance of Location */
+        public static Location locateLargest(double[][] a) {
+            return new Location(a);
+        }
+
 }
